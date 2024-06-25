97cb85c127515473e1b5a80e4fd80b7a8877e373
Mediazen <Mediazen@LAPTOP-J1HJBK0R>
2024-06-25 15:52:03 +0900

    Feat: 스프링 시큐리티 관련 설정 추가
    - csrf() : csrf 보안 비활성화
    - httpBasic(), formLogin(), session : JWT 사용으로 인한 미사용
    - authorizeHttpRequests() : '/api/v1/users/**' 경로만 인증 필요하도록 설정
    - BCryptPasswordEncoder 활용 비밀번호 암호화(해시)

---
+++ tts/src/main/java/kr/co/mediazen/tts/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package kr.co.mediazen.tts.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http
+				.csrf().disable()
+				.httpBasic().disable()
+				.formLogin().disable()
+
+				.authorizeHttpRequests()
+					.antMatchers("/api/v1/users/**").authenticated()
+					.anyRequest().permitAll()
+
+				.and()
+					.formLogin()
+					.loginPage("/api/v1/login")
+					.loginProcessingUrl("/login")
+					.defaultSuccessUrl("/")
+
+				.and()
+					.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		return http.build();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
