404f923463275b4a1429216a229d806b67578c56
Mediazen <Mediazen@LAPTOP-J1HJBK0R>
2024-06-26 12:14:07 +0900

    Feat : Model 추가
    - 부서(Department)와 직위(Role)는 데이터 무결성을 위해 Enum 클래스 생성

---
+++ tts/src/main/java/kr/co/mediazen/tts/model/Department.java
@@ -0,0 +1,7 @@
+package kr.co.mediazen.tts.model;
+
+public enum Department {
+	WEB,
+	AI,
+	SOLUTION
+}
---
+++ tts/src/main/java/kr/co/mediazen/tts/model/Role.java
@@ -0,0 +1,9 @@
+package kr.co.mediazen.tts.model;
+
+public enum Role {
+	PRO,
+	MANAGER,
+	TEAM_LEADER,
+	EXECUTIVE,
+	CEO
+}
