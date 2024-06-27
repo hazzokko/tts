1cc303eb3b255ef1805301d791b2c7a303fa1b39
Mediazen <Mediazen@LAPTOP-J1HJBK0R>
2024-06-27 15:39:33 +0900

    Fix : 회원가입 로직 수정 후 암호화된 비밀번호 setter가 없던 오류를 수정

--- tts/src/main/java/kr/co/mediazen/tts/service/UserServiceImpl.java
+++ tts/src/main/java/kr/co/mediazen/tts/service/UserServiceImpl.java
@@ -4,9 +4,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import kr.co.mediazen.tts.dto.request.UserJoinRequestDto;
-import kr.co.mediazen.tts.mapper.UserMapper;
-import kr.co.mediazen.tts.model.User;
+import kr.co.mediazen.tts.dao.request.UserDAO;
+import kr.co.mediazen.tts.model.UserVO;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -14,26 +13,16 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class UserServiceImpl implements UserService {
 	
-	private final UserMapper userMapper;
+	private final UserDAO userDAO;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Transactional
 	@Override
-	public void join(UserJoinRequestDto dto) {
-
-		// 비밀번호 암호화
-		String rawPassword = dto.getPassword();
+	public void join(UserVO userVO) {
+		String rawPassword = userVO.getPassword();
 		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		userVO.setPassword(encPassword);
 		
-		User user = User.builder()
-				.id(dto.getId())
-				.password(encPassword)
-				.username(dto.getUsername())
-				.department(dto.getDepartment())
-				.phone(dto.getPhone())
-				.role(dto.getRole())
-				.build();
-		
-		userMapper.join(user);
+		userDAO.join(userVO);
 	}
 }
